d0a132b3527f965de98337d8cc96371a3aaa79ff
Pullolian <floriancunat@orange.fr>
2020-01-20 18:09:34 +0100

    Changements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { Menu1Component } from './menu1/menu1.component';
-<<<<<<< HEAD
 import { VoirAdoptionComponent } from './voir-adoption/voir-adoption.component';
+import { AlerteComponent } from './alerte/alerte.component';
 import { HttpClientModule } from '@angular/common/http';
 import { from } from 'rxjs';
-=======
-import { AlerteComponent } from './alerte/alerte.component';
->>>>>>> f18b725da36da30038ce73e59b0bbb97538fc438
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
     Menu1Component,
-<<<<<<< HEAD
-    VoirAdoptionComponent
-=======
+    VoirAdoptionComponent,
     AlerteComponent
->>>>>>> f18b725da36da30038ce73e59b0bbb97538fc438
   ],
   imports: [
     BrowserModule,
