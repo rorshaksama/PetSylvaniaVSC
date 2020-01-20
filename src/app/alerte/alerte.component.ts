297adee3f6344d87a96e0a01f832a81e14514b2c
Pullolian <floriancunat@orange.fr>
2020-01-20 17:16:54 +0100

    juste one commit please

---
+++ src/app/alerte/alerte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alerte',
+  templateUrl: './alerte.component.html',
+  styleUrls: ['./alerte.component.css']
+})
+export class AlerteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { Menu1Component } from './menu1/menu1.component';
+import { AlerteComponent } from './alerte/alerte.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
-    Menu1Component
+    Menu1Component,
+    AlerteComponent
   ],
   imports: [
     BrowserModule,
