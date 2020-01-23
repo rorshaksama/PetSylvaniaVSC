b765fab563c5a8237323ee66be0608168776b80a
cecedev <cel.priol@gmail.com>
2020-01-23 10:14:44 +0100

    Root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {  VoirAdoptionComponent } from './voir-adoption/voir-adoption.component';
+import { GardeComponent } from './garde/garde.component';
 
 
 
 const routes: Routes = [
-  {path: 'voirAdoption', component: VoirAdoptionComponent}
+  {path: 'voirAdoption', component: VoirAdoptionComponent},
+  {path: 'garde', component: GardeComponent}
 ];
 
 
