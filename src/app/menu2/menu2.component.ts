c55f0219ef7c21c993057c30623ff53c490cb6b9
rorshaksama <cmd.rorshak@laposte.net>
2020-01-23 15:23:38 +0100

    menu et app comp

--- src/app/menu2/menu2.component.ts
+++ src/app/menu2/menu2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from '../myservice.service';
 
 @Component({
   selector: 'app-menu2',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu2.component.css']
 })
 export class Menu2Component implements OnInit {
-
-  constructor() { }
+  t;
+  constructor(private myservice: MyserviceService) { }
 
   ngOnInit() {
+  }
+
+  test() {
+    this.t = this.myservice.deconnexion();
   }
 
 }
