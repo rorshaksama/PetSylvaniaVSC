7ecb8e16aef0e679dad0b41a967255588c5ab73a
rorshaksama <cmd.rorshak@laposte.net>
2020-01-24 15:01:51 +0100

    upgrade profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,24 +13,87 @@ export class ProfileComponent implements OnInit {
   user: User = new User();
   u;
   infoConnexion;
-  hsnom = false;
-  myName;
 
-  name: string;
-  prenom: string;
+  myName = false;
+  myPName = true;
+
+  myPrenom = false;
+  myPPrenom = true;
+
+  myMail = false;
+  myPMail = true;
+
+  myLogin = false;
+  myPLogin = true;
+
+  Cname;
+  Cprenom;
+  Cmail;
+  Clogin;
+
+
 
   constructor(private http: HttpClient, private route: Router, private myservice: MyserviceService) { }
 
   ngOnInit() {
     this.u = this.myservice.recupUserConnectLocalStorage();
   }
+  // -------------------- NOM
   hideAndShowNom() {
-    if (this.hsnom === false) {
-      this.hsnom = true;
+    if (this.myName === false) {
+      this.myName = true;
+
+    } else {
+      this.myName = false;
     }
   }
   changeNom(name) {
-    this.myName = name;
+    this.Cname = name;
+    this.myName = false;
+    this.myPName = false;
   }
+  // -------------------- PRENOM
+  hideAndShowPrenom() {
+    if (this.myPrenom === false) {
+      this.myPrenom = true;
+
+    } else {
+      this.myPrenom = false;
+    }
+  }
+  changePrenom(prenom) {
+    this.Cprenom = prenom;
+    this.myPrenom = false;
+    this.myPPrenom = false;
+  }
+  // -------------------- MAIL
+  hideAndShowMail() {
+    if (this.myMail === false) {
+      this.myMail = true;
+
+    } else {
+      this.myMail = false;
+    }
+  }
+  changeMail(mail) {
+    this.Cmail = mail;
+    this.myMail = false;
+    this.myPMail = false;
+  }
+  // ------------------- LOGIN
+  hideAndShowLogin() {
+    if (this.myLogin === false) {
+      this.myLogin = true;
+
+    } else {
+      this.myLogin = false;
+    }
+  }
+  changeLogin(login) {
+    this.Clogin = login;
+    this.myLogin = false;
+    this.myPLogin = false;
+  }
+
 
 }
